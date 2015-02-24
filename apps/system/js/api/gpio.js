1169a4ab9ce06c34f97c4f6d549a85808e9e5aef
Jan Jongboom <janjongboom@gmail.com>
2015-02-24 18:49:21 +0100

    Make proper demo for gpio

--- apps/system/js/api/gpio.js
+++ apps/system/js/api/gpio.js
@@ -1,44 +1,71 @@
+function _write(value, path) {
+  return navigator.mozOs.openFile(path, 'wb').then(file => {
+    return Promise.resolve()
+      .then(() => file.write(value, 'utf-8'))
+      .then(() => file.close());
+  });
+}
+
+function _read(path, bytes) {
+  return navigator.mozOs.openFile(path).then(file => {
+    var data;
+    return Promise.resolve()
+      // @todo, its not utf-8 i think
+      .then(() => file.read(bytes || Number.Infinity, 'utf-8'))
+      .then(aData => data = aData)
+      .then(() => file.close())
+      .then(() => data);
+  });
+}
+
 navigator.gpio = {
   _pins: {},
-
+  
   getPin: function(id) {
     return this._pins[id];
   },
 
   setPinMode: function(id, mode) {
-    var self = this;
-    
     var direction;
     switch (mode) {
+      case 'in':
+      case 'input':
+        direction = 'in';
+        break;
+      case 'out':
       case 'output':
         direction = 'out';
         break;
       default:
-        throw 'Only support output';
+        throw 'mode should be in|out';
     }
     
-    return new Promise((res, rej) => {
-      navigator.mozOs.exec('echo ' + id + ' > /sys/class/gpio/export')
-        .then(() => navigator.mozOs.exec('echo ' + direction + ' > /sys/class/gpio/gpio' + id + '/direction'))
-        .then(obj => {
-          if (obj.exitCode === 0) {
-            self._pins[id] = new GpioPin(id, mode, direction);
-            res(self._pins[id]);
-          }
-          else {
-            console.error('Writing gpio failed', obj);
-            rej('exitCode is ' + obj.exitCode);
-          }
-        }, rej);
-    });
+    var promise = 
+      _write(id, '/sys/class/gpio/export')
+        .then(() => _write(direction, '/sys/class/gpio/gpio' + id + '/direction'))
+        .then(() => {
+          var fm = direction === 'out' ? 'wb' : 'rb';
+          return navigator.mozOs.openFile('/sys/class/gpio/gpio' + id + '/value', fm);
+        })
+        .then(fd => {
+          console.log('hi there', id, fd);
+          this._pins[id] = new GpioPin(id, mode, direction, fd);
+          return this._pins[id];
+        })
+        .catch(err => {
+          console.error('Exporting gpio pin', id, 'failed', err);
+          throw err;
+        });
+        
+    return promise;
   }
 };
 
-function GpioPin(id, mode, direction) {
+function GpioPin(id, mode, direction, fd) {
   this.id = id;
   this.mode = mode;
   this.direction = direction;
-  this.path = '/sys/class/gpio/gpio' + id + '';
+  this.fd = fd;
 }
 
 GpioPin.prototype.addEventListener = function() {
@@ -48,28 +75,29 @@ GpioPin.prototype.removeEventListener = function() {
   throw 'NotImplemented';
 };
 GpioPin.prototype.writeDigital = function(value) {
-  value = value ? '1' : '0';
-  
-  // console.log('executing', 'echo ' + value + ' > ' + this.path + '/value');
-  
-  return navigator.mozOs.exec('echo ' + value + ' > ' + this.path + '/value')
-    .then(obj => {
-      // console.log('response is', obj);
-      if (obj.exitCode === 0) {
-        return true;
-      }
-      else {
-        console.error('writeDigital failed', obj);
-        throw 'exitCode is not 0 ' + obj.exitCode;
-      }
-    }).catch(err => console.log('err', err));
+  value = value ? 1 : 0;
+
+  return Promise.resolve()
+    .then(() => this.fd.setPosition(0, 'POS_START'))
+    .then(() => this.fd.write(value, 'utf-8'))
+    .catch(err => {
+      this._lastValue = null;
+      console.error('Gpio', this.id, 'writeDigital failed', err);
+      throw err;
+    });
 };
 GpioPin.prototype.writeAnalog = function() {
   throw 'NotImplemented';
 };
 GpioPin.prototype.readDigital = function() {
-  throw 'NotImplemented';
+  return Promise.resolve()
+    .then(() => this.fd.setPosition(0, 'POS_START'))
+    .then(() => this.fd.read(1, 'utf-8'))
+    .then(res => res[0] === '1' ? true : false);
 };
 GpioPin.prototype.readAnalog = function() {
   throw 'NotImplemented';
+};
+GpioPin.prototype.release = function() {
+  return this.fd.close();
 };
--- apps/system/js/my-app.js
+++ apps/system/js/my-app.js
@@ -1,21 +1,52 @@
-var gpio = navigator.gpio;
+/**
+ * A simple Firefox OS Script to deal with GPIO
+ */
+window.addEventListener('ready', () => {
 
-Promise.all([
-  gpio.setPinMode(2, 'output'),
-  gpio.setPinMode(3, 'output')
-]).then(pins => {
-  
-  let [ pin2, pin3 ] = pins;
-  
-  var value = 0;
-  function go() {
-    value ^= 1;
-    
-    Promise.all([
-      pin2.writeDigital(value),
-      pin3.writeDigital(value ^ 1)
-    ]).then(go, err => console.error('Write failed', err));
-  }
-  go();
-  
+  // First we map our pins to be input / output pins
+  // We don't support pullup/pulldown, you need to do that with hardware
+  Promise.all([
+    navigator.gpio.setPinMode(2, 'output'),
+    navigator.gpio.setPinMode(3, 'output'),
+    navigator.gpio.setPinMode(26, 'input')
+  ]).then(pins => {
+
+    // Here you have a reference to your pins again
+    // You can also use navigator.gpio.getPin(ID)
+    let [ pin2, pin3, pin26 ] = pins;
+
+    // Example 1: Blink a LED every 500 ms. (after pressing the button)
+    let blinkTimeout;
+    let blinkValue = 0;
+
+    function blink() {
+      blinkValue ^= 1;
+      pin2.writeDigital(blinkValue);
+      
+      blinkTimeout = setTimeout(blink, Number(document.querySelector('#speed').value));
+    }
+
+    document.querySelector('.blink').onclick = () => {
+      if (blinkTimeout) {
+        return clearTimeout(blinkTimeout);
+      }
+      blink();
+    };
+
+    document.querySelector('#speed').oninput = e => {
+      document.querySelector('label[for=speed]').textContent = e.target.value;
+    };
+
+    // Example 2: Turn on a LED when button is pressed
+    // This happens in a loop as there is no hardware interrupts (yet?)
+    setInterval(() => {
+
+      // Read it, then pass the value on to pin3
+      pin26.readDigital().then(v => pin3.writeDigital(v))
+        .catch(err => console.error(err));
+
+    }, 50);
+
+  });
+
 });
